2caa6db59efe43257c2fb9528ee1623975f5b7de
rinesh007 <chaulagairinesh7@gmail.com>
2024-06-05 11:55:13 +0545

    env variable

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,24 +1,61 @@
-import React from 'react'
-import './weather.css'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-import search_icon from '../assets/search.png'
-
+import React, { useEffect } from 'react';
+import "./weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+import { clear } from "@testing-library/user-event/dist/clear";
 
 const Weather = () => {
-  return (
-    <div className='weather'>
-        <div className='search-bar'>
-            <input type="text" placeholder='Search' />
-            <img src= {search_icon} alt="" />
-        </div>
-        
-    </div>
-  )
-}
+  const search = async (city)=>{
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
 
-export default Weather
+      const response = await fetch(url);
+
+      const data = await response.json();
+      console.log(data);  
+    } catch (error) {
+      console.log(error.message);  
+
+
+    }
+  }
+  useEffect(()=>{
+    search("London");
+  },[])
+
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} />
+      </div>
+      <img src={clear_icon} className="weather-icon" />
+      <p className="temperature">16°C</p>
+      <p className="location">London</p>
+      <div className="weather-data">
+        <div className="col">
+          <img src={humidity_icon} alt="" />
+          <div>
+            <p>91 %</p>
+            <span>Humidity</span>
+          </div>
+        </div>
+        <div className="col">
+          <img src={wind_icon} alt="" />
+          <div>
+            <p>3.6 Km/h</p>
+            <span>Wind Speed</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
